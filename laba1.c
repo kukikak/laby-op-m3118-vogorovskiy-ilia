79b6180b9b8dfa97318dc9f56c4f2ff58f15cb7c
kukikak <semstar300405@gmail.com>
2022-12-30 17:21:10 +0300

    Create laba1.c

---
+++ laba1.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+int main()
+{
+ int a;
+ char b[100];
+ printf("vvedite chislo a=");
+ scanf("%d",&a);
+ printf("a=%d\n",a);
+ printf("vvedite stroku b=");
+ scanf("%s",&b);
+ printf("%s",b);
+ return 0;
+}
